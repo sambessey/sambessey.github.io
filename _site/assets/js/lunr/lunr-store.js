06c5b04a57a7927814351172e73253c578ed0fab
sambessey <sam@sambesey.com>
2019-03-10 15:17:55 +0000

    Vongole Online

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,11 @@
+var store = [{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"Welcome   Hello world, this is my first Jekyll blog post.   I hope you like it!  ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/First-Post/",
+        "teaser":null},{
+        "title": "What is the notion of ‘taking back control’?",
+        "excerpt":"Is it just a soundbite? The core of both the Government’s Brexit rhetoric and a lot of leaver’s beliefs is the notion of ‘Taking Back Control’. What does this really mean? I’ve personally asked Brexiteers and I am yet to get a straight answer on this. The most common answers...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/Test-Blog/",
+        "teaser":null}]
